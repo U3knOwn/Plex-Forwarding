d9761f995aac72aebbb16d7293a97222ef51ff06
Jamal Rothfuchs <148533561+U3knOwn@users.noreply.github.com>
2024-03-22 18:38:01 +0100

    Add logic to remove query and space when it is recognized in the URL

--- app/src/main/java/de/u3kn0wn/forwarding/MainActivity.kt
+++ app/src/main/java/de/u3kn0wn/forwarding/MainActivity.kt
@@ -17,15 +17,31 @@ class MainActivity : Activity() {
         if (intent != null && Intent.ACTION_SEND == intent.action && intent.type?.startsWith("text/") == true) {
             val sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
             if (sharedText != null) {
-                val url = Uri.parse(sharedText)
+                val modifiedText = removeWhiteSpace(sharedText)
+                val modifiedUrl = removeQueryStringParameterIfPresent(modifiedText)
+                val url = Uri.parse(modifiedUrl)
                 val openUrlIntent = Intent(Intent.ACTION_VIEW, url)
                 if (openUrlIntent.resolveActivity(packageManager) != null) {
                     startActivity(openUrlIntent)
-                    Log.d(localClassName, "Output: $sharedText")
+                    Log.d(localClassName, "Output: $modifiedUrl")
                 }
             } else {
                 Log.d(localClassName, "Text is null or empty")
             }
         }
+    }
+
+    private fun removeWhiteSpace(input: String): String {
+        return input.replace("\\s+".toRegex(), "")
+    }
+
+    private fun removeQueryStringParameterIfPresent(url: String): String {
+        val parametersToRemove = (0..9).map { "i=$it" }
+        var modifiedUrl = url
+        for (parameterToRemove in parametersToRemove) {
+            modifiedUrl = modifiedUrl.replace("&$parameterToRemove", "")
+            modifiedUrl = modifiedUrl.replace("?$parameterToRemove", "")
+        }
+        return modifiedUrl
     }
 }
